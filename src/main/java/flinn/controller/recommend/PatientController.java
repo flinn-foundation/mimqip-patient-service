b71728fcaeec88648f36d3798fc4b71633295a62
Michael Iden <michaeliden90@gmail.com>
2017-03-31 11:02:41 -0400

    Controller was non-functional without the path variable annotation

--- src/main/java/flinn/controller/recommend/PatientController.java
+++ src/main/java/flinn/controller/recommend/PatientController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,12 +25,11 @@ public class PatientController implements PatientsApi {
 
     @Override
     public ResponseEntity<List<PatientDto>> patientsGet() {
-        log.info("HERE");
         return new ResponseEntity<>(patientService.getPatientsData(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<PatientDto> patientsIdGet(Long id) {
+    public ResponseEntity<PatientDto> patientsIdGet(@PathVariable("id") Long id) {
         return ResponseEntity.ok(patientService.getPatientData(id));
     }
 }
